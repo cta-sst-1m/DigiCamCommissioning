6beca28540ee92ad7320ea69dd770a623f2fc4d8
Victor Coco <victor.coco@cern.ch>
2017-01-30 14:39:55 +0100

    Added MC geometry

--- data_treatement/adc_hist.py
+++ data_treatement/adc_hist.py
@@ -1,6 +1,9 @@
 import numpy as np
 from ctapipe.io import zfits
 from utils.peakdetect import spe_peaks_in_event_list
+from utils.toy_reader import ToyReader
+import logging
+import sys
 
 
 def run(hist, options, h_type='ADC', prev_fit_result=None):
@@ -12,30 +15,33 @@ def run(hist, options, h_type='ADC', prev_fit_result=None):
     :param prev_fit_result: fit result of a previous step needed for the calculations
     :return:
     """
+    logger = logging.getLogger(sys.modules['__main__'].__name__ + __name__)
     # Reading the file
     n_evt, n_batch, batch_num, max_evt = 0, options.n_evt_per_batch, 0, options.evt_max
     batch = None
 
-    print('--|> Treating the batch #%d of %d events' % (batch_num, n_batch))
+    logger.info('Treating the batch #%d of %d events' % (batch_num, n_batch))
     for file in options.file_list:
         # Open the file
         _url = options.directory + options.file_basename % file
-        inputfile_reader = zfits.zfits_event_source(
-            url=_url
-            , data_type='r1', max_events=options.evt_max)
+        if not options.mc:
+            inputfile_reader = zfits.zfits_event_source(url=_url, data_type='r1', max_events=100000)
+        else:
+            inputfile_reader = ToyReader(filename=_url, id_list=[0],
+                                         max_events=options.evt_max,
+                                         n_pixel=options.n_pixels)
 
-        if options.verbose:
-            print('--|> Moving to file %s' % _url)
+        logger.info('--|> Moving to file %s' % _url)
         # Loop over event in this file
         for event in inputfile_reader:
             n_evt += 1
             if n_evt > max_evt:
                 break
             if (n_evt - n_batch * batch_num) % n_batch / 100 == 0:
-                print("Progress {:2.1%}".format(float(n_evt - batch_num * n_batch) / n_batch), end="\r")
+                logger.info("Progress {:2.1%}".format(float(n_evt - batch_num * n_batch) / n_batch), end="\r")
             for telid in event.r1.tels_with_data:
                 if n_evt % n_batch == 0:
-                    print('--|> Treating the batch #%d of %d events' % (batch_num, n_batch))
+                    logger.info('Treating the batch #%d of %d events' % (batch_num, n_batch))
                     # Update adc histo
                     if h_type == 'ADC':
                         hist.fill_with_batch(batch.reshape(batch.shape[0], batch.shape[1] * batch.shape[2]))
@@ -45,7 +51,7 @@ def run(hist, options, h_type='ADC', prev_fit_result=None):
                     # Reset the batch
                     batch = None
                     batch_num += 1
-                    print('--|> Reading  the batch #%d of %d events' % (batch_num, n_batch))
+                    logger.info('Reading  the batch #%d of %d events' % (batch_num, n_batch))
                 # Get the data
                 data = np.array(list(event.r1.tel[telid].adc_samples.values()))
                 # Append the data to the batch
@@ -54,7 +60,4 @@ def run(hist, options, h_type='ADC', prev_fit_result=None):
                 else:
                     batch = np.append(batch, data.reshape(data.shape[0], 1, data.shape[1]), axis=1)
 
-    if options.verbose:
-        print('--|> Save the data in %s' % (options.output_directory + options.histo_filename))
-    np.savez_compressed(options.output_directory + options.histo_filename,
-                        adcs=hist.data, adcs_bin_centers=hist.bin_centers)
+    return
