35b3fdaa658a9125507d457d1a2829e8ca2e6d95
Victor Coco <victor.coco@cern.ch>
2017-01-30 14:52:35 +0100

    Started modifications: - logger - save histo - structure (analysis) -configuration ==> applied to hvoff only for the moment To be tested

--- utils/histogram.py
+++ utils/histogram.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize
 from numpy.linalg import inv
+import logging,sys,os
 
 class Histogram:
     """
@@ -13,7 +14,8 @@ class Histogram:
 
     def __init__(self, data: np.array = np.zeros(0), data_shape: tuple = (0,), bin_centers: np.array = np.zeros(0), bin_center_min: int = 0,
                  bin_center_max: int = 1,
-                 bin_width: int = 1, xlabel: str = 'x', ylabel: str = 'y', label: str = 'hist'):
+                 bin_width: int = 1, xlabel: str = 'x', ylabel: str = 'y', label: str = 'hist',
+                 filename : str = ''):
         """
         Initialise method
 
@@ -34,8 +36,17 @@ class Histogram:
         :param ylabel: the y axis label
         :param label: the base label for the histograms
         """
+        # Initialise the logger
+        self.logger = logging.getLogger(sys.modules['__main__'].__name__+__name__)
+
+        if filename :
+            self.load(filename)
+            return
+
         # Initialisation of the bin_centers
         if bin_centers.shape[0] == 0:
+            self.logger.debug('Generate histogram axis from bin centers limits')
+
             self.bin_width = bin_width
             # generate the bin edge array
             self.bin_edges = np.arange(bin_center_min - self.bin_width / 2, bin_center_max + self.bin_width / 2 + 1,
@@ -43,6 +54,8 @@ class Histogram:
             # generate the bin center array
             self.bin_centers = np.arange(bin_center_min, bin_center_max + 1, self.bin_width)
         else:
+            self.logger.debug('Generate histogram axis from bin centers')
+
             self.bin_centers = bin_centers
             # generate the bin edge array
             self.bin_width = self.bin_centers[1] - self.bin_centers[0]
@@ -52,20 +65,99 @@ class Histogram:
 
         # Initialisation of data
         if data.shape[0] == 0:
-            self.data = np.zeros(data_shape + (self.bin_centers.shape[0],))
+            self.logger.debug('Initialise histogram')
+            self.data      = np.zeros(data_shape + (self.bin_centers.shape[0],))
+            self.underflow = np.zeros(data_shape )
+            self.overflow  = np.zeros(data_shape )
             self.errors = np.zeros(data_shape + (self.bin_centers.shape[0],))
         else:
+            self.logger.debug('Initialise histogram from existing data')
             self.data = data
+            self.underflow = np.zeros(data_shape )
+            self.overflow  = np.zeros(data_shape )
             self._compute_errors()
 
         # Initialisation of fit results and labels
         self.fit_result = None
+        self.fit_result_label = None
         self.fit_chi2_ndof = None
         self.fit_function = None
         self.fit_axis = None
         self.xlabel = xlabel
         self.ylabel = ylabel
         self.label = label
+        return
+
+    def save(self, filename):
+        """
+        Save the histogram and its properties in a npz file
+
+        :param filename: the full path of the saved histogram
+        :return:
+        """
+        if not os.path.isdir( os.path.dirname(filename) ):
+            self.logger.critical('%s does not exist'%os.path.dirname(filename))
+            raise FileNotFoundError
+        try:
+            np.savez_compressed(filename,
+                                data=self.data,
+                                bin_centers=self.bin_centers,
+                                bin_edges=self.bin_edges,
+                                bin_width=np.array([self.bin_width]),
+                                errors=self.errors,
+                                underflow=self.underflow,
+                                overflow=self.overflow,
+                                fit_result=self.fit_result,
+                                fit_function_name=np.array([self.fit_function.__name__]),
+                                fit_chi2_ndof=self.fit_chi2_ndof,
+                                fit_axis=self.fit_axis,
+                                xlabel=np.array([self.xlabel]),
+                                ylabel=np.array([self.ylabel]),
+                                label=np.array([self.label]),
+                                fit_result_label=self.fit_result_label)
+            self.logger.info('Saved histogram in %s' % filename)
+        except Exception as inst:
+            self.logger.critical('Could not save in %s'%filename, inst)
+            raise Exception(inst)
+
+    def load(self, filename):
+        """
+        Save the histogram and its properties in a npz file
+
+        :param filename: the full path of the saved histogram
+        :return:
+        """
+
+        if not os.path.isfile( filename ):
+            self.logger.critical('%s does not exist'%filename)
+            raise FileNotFoundError
+
+        try :
+            file = np.load(filename)
+
+            self.data = np.copy(file['data'])
+            self.bin_centers = np.copy(file['bin_centers'])
+            self.bin_edges = np.copy(file['bin_edges'])
+            self.bin_width = np.copy(file['bin_width'][0])
+            self.errors = np.copy(file['errors'])
+            self.underflow = np.copy(file['underflow'])
+            self.overflow = np.copy(file['overflow'])
+            self.fit_result = np.copy(file['fit_result'])
+            self.fit_function_name = np.copy(file['fit_function_name'][0])
+            self.fit_chi2_ndof = np.copy(file['fit_chi2_ndof'])
+            self.fit_axis = np.copy(file['fit_axis'])
+            self.xlabel = np.copy(file['xlabel'][0])
+            self.ylabel = np.copy(file['ylabel'][0])
+            self.label = np.copy(file['label'][0])
+            self.fit_result_label = np.copy(file['fit_result_label'])
+
+            file.close()
+
+        except Exception as inst:
+            self.logger.critical('Could not load %s' % filename, inst)
+            raise Exception(inst)
+
+        return
 
     def fill(self, value, indices=None):
         """
@@ -74,15 +166,20 @@ class Histogram:
         :param indices:
         :return: void
         """
+        # TODO deal with underflow and overflow and do the doc + optimize the function
+
         # change the value array to an array of Histogram index to be modified
         hist_indices = ((value - self.bin_edges[0]) // self.bin_width).astype(int)
+
         # treat overflow and underflow
         hist_indices[hist_indices > self.data.shape[-1] - 1] = self.data.shape[-1] - 1
         hist_indices[hist_indices < 0] = 0
+
         # get the corresponding indices multiplet
         dim_indices = tuple([np.indices(value.shape)[i].reshape(np.prod(value.shape)) for i in
                              range(np.indices(value.shape).shape[0])], )
         dim_indices += (hist_indices.reshape(np.prod(value.shape)),)
+
         if value[..., 0].shape == self.data[..., 0].shape or not indices:
             self.data[dim_indices] += 1
         else:
@@ -96,32 +193,51 @@ class Histogram:
         :return:
         """
         # noinspection PyUnusedLocal
-        data = None
+        data, underflow, overflow = None,None,None
         if not indices:
             data = self.data
+            underflow = self.underflow
+            overflow = self.overflow
         else:
             data = self.data[indices]
+            underflow = self.underflow[indices]
+            overflow = self.overflow[indices]
 
+        # create the histograms out of the data
         hist = lambda x: np.histogram(x, bins=self.bin_edges, density=False)[0]
 
         if batch.dtype != 'object':
             # Get the new Histogram
-            new_hist = np.apply_along_axis(hist, len(data.shape) - 1, batch)
-            # Add it to the existing
-            data = np.add(data, new_hist)
-
+            tmp_hist = np.apply_along_axis(hist, len(data.shape) - 1, batch)
+            # Get the overflow and underflow data
+            tmp_underflow = np.sum(~(batch > self.bin_edges[0]), axis = -1)
+            tmp_overflow = np.sum(~(batch < self.bin_edges[1]), axis = -1)
+            # Add it to the existing datas
+            data = np.add(data, tmp_hist)
+            underflow = np.add(underflow, tmp_underflow)
+            overflow  = np.add(overflow , tmp_overflow)
         else:
             for index in np.ndindex(batch.shape):
                 # Get the new Histogram
-                new_hist = hist(batch[index])
+                tmp_hist = hist(batch[index])
+                # Get the overflow and underflow data
+                tmp_underflow = np.sum(~(batch[index] > self.bin_edges[0]))
+                tmp_overflow = np.sum(~(batch[index] < self.bin_edges[1]))
                 # Add it to the existing
-                data[index] = np.add(data[index], new_hist)
+                data[index] = np.add(data[index], tmp_hist)
+                underflow[index] = np.add(underflow[index], tmp_underflow)
+                overflow[index] = np.add(overflow[index], tmp_overflow)
 
+        # Feed it back to the object
         if not indices:
             self.data = data
+            self.underflow = underflow
+            self.overflow = overflow
         else:
             self.data[indices] = data
-
+            self.underflow[indices] = underflow
+            self.overflow[indices] = overflow
+        # compute the poisson error on the data
         self._compute_errors()
 
     @staticmethod
@@ -129,16 +245,21 @@ class Histogram:
         """
         Return the residuals of the data with respect to a function
 
-        :param function: The function (defined with arguments params and x)
-        :param p: the parameters of the function (np.array)
-        :param x: the x values (np.array)
-        :param y: the y values (np.array)
-        :param y_err: the y values errors (np.array)
-        :return: the residuals (np.array)
+        :param function: The function defined with arguments params and x (function)
+        :param p: the parameters of the function                          (np.array)
+        :param x: the x values                                            (np.array)
+        :param y: the y values                                            (np.array)
+        :param y_err: the y values errors                                 (np.array)
+        :return: the residuals                                            (np.array)
         """
         return (y - function(p, x)) / y_err
 
     def _compute_errors(self):
+        """
+        Compute poisson error of the sample
+
+        :return:
+        """
         self.errors = np.sqrt(self.data)
         self.errors[self.errors == 0.] = 1.
 
@@ -146,14 +267,15 @@ class Histogram:
         """
         Perform a fit on this specific Histogram
 
-        :param idx:      the index of the Histogram in self.data        (type: tuple)
-        :param func:     the fit function                               (type: function)
-        :param p0:       the initial fit parameters                     (type: list)
-        :param slice_list:    the slice_list of data to fit                       (type list)
-        :param bounds:   the boundary for the parameters                (type tuple(list,list))
-        :param fixed_param: the parameters to be fixed and their values (type list(list,list))
-        :return: the fit result                                         (type np.array)
+        :param idx:      the index of the Histogram in self.data        (tuple)
+        :param func:     the fit function                               (function)
+        :param p0:       the initial fit parameters                     (list)
+        :param slice_list:    the slice_list of data to fit             (list)
+        :param bounds:   the boundary for the parameters                (tuple(list,list))
+        :param fixed_param: the parameters to be fixed and their values (list(list,list))
+        :return: the fit result                                         (np.array)
         """
+        # TODO inline comment of the function
         # Reduce the functions parameters according to the fixed_param
         reduced_p0 = p0
         reduced_bounds = bounds
@@ -176,7 +298,6 @@ class Histogram:
             for i, param in enumerate(p0):
                 if not (i in fixed_param[0]):
                     reduced_p0 += [param]
-            # print('red_p0',reduced_p0)
 
             reduced_bounds = [[], []]
             for i, param in enumerate(p0):
@@ -184,15 +305,14 @@ class Histogram:
                     reduced_bounds[0] += [bounds[0][i]]
                     reduced_bounds[1] += [bounds[1][i]]
             reduced_bounds = tuple(reduced_bounds)
-            # print('red_bound',reduced_bounds)
         # noinspection PyUnusedLocal
         fit_result = None
         if slice_list == [0, 0, 1] or self.data[idx][slice_list[0]:slice_list[1]:slice_list[2]].shape == 0 \
                 or np.any(np.isnan(reduced_p0)) \
                 or np.any(np.isnan(reduced_bounds[0])) or np.any(np.isnan(reduced_bounds[1])) \
                 or np.any(np.isnan(p0)):
-            if verbose:
-                print('Bad inputs')
+
+            self.logger.debug('Bad inputs')
             fit_result = (np.ones((len(reduced_p0), 2)) * np.nan)
             ndof = (slice_list[1] - slice_list[0]) / slice_list[2] - len(reduced_p0)
             chi2 = np.nan
@@ -216,18 +336,17 @@ class Histogram:
                     cov = np.sqrt(np.diag(inv(np.dot(out.jac.T, out.jac))))
                     fit_result = np.append(val.reshape(val.shape + (1,)), cov.reshape(cov.shape + (1,)), axis=1)
                 except np.linalg.linalg.LinAlgError as inst:
-                    if verbose:
-                        print(inst)
-                    print('Could not compute errors')
-                    print(inst)
+                    self.logger.warning('Could not compute error in the fit of',idx)
+                    self.logger.debug(inst)
                     fit_result = np.append(val.reshape(val.shape + (1,)), np.ones((len(reduced_p0), 1)) * np.nan,
                                            axis=1)
 
             except Exception as inst:
-                print('failed fit', inst, 'index', idx)
-                print('p0', reduced_p0)
-                print('bound min', reduced_bounds[0])
-                print('bound max', reduced_bounds[1])
+                self.logger.error('Could not fit index', idx)
+                self.logger.error(inst)
+                self.logger.debug('p0:', reduced_p0)
+                self.logger.debug('bound min:', reduced_bounds[0])
+                self.logger.debug('bound max:', reduced_bounds[1])
                 fit_result = (np.ones((len(reduced_p0), 2)) * np.nan)
 
         # restore the fixed_params in the fit_result
@@ -237,13 +356,14 @@ class Histogram:
         return fit_result, chi2, ndof
 
     # noinspection PyDefaultArgument
-    def fit(self, func, p0_func, slice_func, bound_func, config=None, limited_indices=None, fixed_param=[],
+    def fit(self, func, p0_func, slice_func, bound_func, labels_func = None , config=None, limited_indices=None, fixed_param=[],
             force_quiet=False):
         """
         An helper to fit Histogram
         :param p0_func:
         :param slice_func:
         :param bound_func:
+        :param labels:
         :param config:
         :param limited_indices:
         :param fixed_param:
@@ -251,6 +371,7 @@ class Histogram:
         :param func:
         :return:
         """
+        # todo COMMENTS
         data_shape = list(self.data.shape)
         data_shape.pop()
         data_shape = tuple(data_shape)
@@ -274,7 +395,7 @@ class Histogram:
                 self.fit_chi2_ndof = np.ones(data_shape + (2,)) * np.nan
 
             if not force_quiet:
-                print("Fit Progress {:2.1%}".format(count / np.prod(data_shape)), end="\r")
+                self.logger.info("Fit Progress {:2.1%}".format(count / np.prod(data_shape)), end="\r")
             count += 1
             # noinspection PyUnusedLocal
             fit_res = None
@@ -296,13 +417,12 @@ class Histogram:
                                                                            config=None),
                                                      bounds=bound_func(self.data[indices], self.bin_centers,
                                                                        config=None),
+                                                     labels = labels_func(self.data[indices], self.bin_centers,
+                                                                       config=None),
                                                      fixed_param=list_fixed_param)
 
             else:
                 func_reduced = lambda _p, x: func(_p, x, config=config[indices])
-                # print('slice',slice_func(self.data[indices], self.bin_centers, config=config[indices]))
-                # print('bounds',bound_func(self.data[indices], self.bin_centers,config=config[indices]))
-                # print('fixed_param',list_fixed_param)
 
                 fit_res, chi2, ndof = self._axis_fit(indices, func_reduced,
                                                      p0_func(self.data[indices], self.bin_centers,
@@ -332,10 +452,29 @@ class Histogram:
             self.fit_chi2_ndof[indices][1] = ndof
 
     def find_bin(self, x):
+        """
+        Function to retreive the bin number
+
+        :param x: value       (float)
+        :return: bin number   (int)
+        """
         return (x - self.bin_edges[0]) // self.bin_width
 
     def show(self, which_hist=None, axis=None, show_fit=False, slice_list=None, config=None, scale='linear', color='k',
              set_ylim=True):
+        """
+        Generic display function
+
+        :param which_hist:
+        :param axis:
+        :param show_fit:
+        :param slice_list:
+        :param config:
+        :param scale:
+        :param color:
+        :param set_ylim:
+        :return:
+        """
 
         if not which_hist:
             which_hist = (0,) * len(self.data[..., 0].shape)
