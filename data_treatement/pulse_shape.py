fca315473b2d0775ef3c493d67dfb45efb3b77e9
calispac <cyril.alispach@unige.ch>
2017-06-01 11:48:03 +0200

    added mc_reader

--- data_treatement/pulse_shape.py
+++ data_treatement/pulse_shape.py
@@ -1,11 +1,15 @@
 # external modules
 import numpy as np
 from ctapipe.io import zfits
+import sys
+import logging
 
 # internal modules
-from utils.toy_reader import ToyReader
 from utils.event_iterator import EventCounter
 from utils.logger import TqdmToLogger
+from data_treatement.generic import subtract_baseline, integrate
+from utils.histogram import Histogram
+from utils.mc_reader import hdf5_mc_event_source
 
 
 def run(options):
@@ -15,40 +19,50 @@ def run(options):
     tqdm_out = TqdmToLogger(log, level=logging.INFO)
     event_counter = EventCounter(options.min_event, options.max_event, log)
 
+    # Get the baseline parameters if running in per event baseline subtraction mode
+    params = None
+    if hasattr(options, 'baseline_per_event_limit'):
+        params = Histogram(filename=options.output_directory + options.baseline_param_data, fit_only=True).fit_result
+        # Initialise the baseline holder
+        baseline = np.zeros((len(options.pixel_list),), dtype=float)
 
     for file in options.file_list:
 
         # Get the file
         _url = options.directory + options.file_basename % file
 
-        if not options.mc:
-            inputfile_reader = zfits.zfits_event_source(url=_url, max_events=len(options.scan_level)*options.events_per_level)
+        if options.mc:
+
+            inputfile_reader = hdf5_mc_event_source(url=_url, max_event=options.max_event, events_per_dc_level=options.max_event, events_per_ac_level=0, dc_start=0, ac_start=0)
+
         else:
-            seed = 0
-            inputfile_reader = ToyReader(filename=_url, id_list=[0], seed=seed, max_events=len(options.scan_level)*options.events_per_level, n_pixel=options.n_pixels, events_per_level=options.events_per_level, level_start=options.scan_level[0])
+
+            inputfile_reader = zfits.zfits_event_source(url=_url, max_events=options.max_event)
 
         if options.verbose:
+
             log.debug('--|> Moving to file %s' % _url)
+
         # Loop over event in this file
         for event, counter in zip(inputfile_reader, event_counter):
+
             if counter.continuing:
                 continue
 
             for telid in event.r0.tels_with_data:
 
-                # get the data
+                # Get the data
                 data = np.array(list(event.r0.tel[telid].adc_samples.values()), dtype=float)
-
-                # get data of selected pixels
+                # Get data of selected pixels
                 data = data[options.pixel_list]
+                # Subtract the baseline and get rid of unwanted pixels
+                data, baseline = subtract_baseline(data, counter.event_id, options, params, baseline)
+                # Perform integration
+                data = integrate(data, options)
+                # Initialise pulse_shape
+                if counter.event_id == 0:
+                    pulse_shape = np.zeros(data.shape)
 
-                # subtract the pedestals
-                data = data - np.mean(data[:,0:options.n_bins_before_signal], axis=-1)[...,None]
+                pulse_shape += data
 
-
-                pulse_shapes[level, :, :, 0] += data/options.events_per_level
-                pulse_shapes[level, :, :, 1] += (data*data)/options.events_per_level
-
-    pulse_shapes[:, :, :, 1] = np.sqrt((pulse_shapes[:, :, :, 1] - pulse_shapes[:,:,:,0]**2) /options.events_per_level)
-
-    return
+    return pulse_shape / event_counter.event_count
